233f9c06d564520b0b6fc6755bb340b833b56ce5
mattolivr <matheusdeoliveira310@gmail.com>
2025-12-05 20:23:12 -0300

    2-DR2-TP2.03

--- 2-desenvolvimento-front-end/DR2-fundamentos-de-react/DR2-TP2.00-biblioteca-crud/src/App.jsx
+++ 2-desenvolvimento-front-end/DR2-fundamentos-de-react/DR2-TP2.00-biblioteca-crud/src/App.jsx
@@ -2,11 +2,39 @@ import './App.css'
 import Header from './components/header'
 
 function App() {
+  const livros = retornaLivros();
+
   return (
     <>
       <Header />
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Autor</th>
+            <th>Ano</th>
+          </tr>
+        </thead>
+        <tbody>
+          {livros.map((livro, index) => (
+            <tr key={index}>
+              <td>{livro.titulo}</td>
+              <td>{livro.autor}</td>
+              <td>{livro.ano}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
+}
+
+function retornaLivros() {
+  return [
+    { titulo: "A Menina que Roubava Livros", autor: "Markus Zusak", ano: 2005 },
+    { titulo: "Admirável Mundo Novo", autor: "Aldous Huxley", ano: 1932 },
+    { titulo: "O Senhor dos Anéis", autor: "J.R.R. Tolkien", ano: 1954 }
+  ]
 }
 
 export default App
