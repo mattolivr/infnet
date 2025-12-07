0f17d410a44907ca10af481357ee3a9365221447
mattolivr <matheusdeoliveira310@gmail.com>
2025-12-07 19:08:12 -0300

    2-DR1-TP3.10-fix

--- 2-desenvolvimento-front-end/DR1-mobile-first-ui/DR1-TP3.10/src/components/Header/index.jsx
+++ 2-desenvolvimento-front-end/DR1-mobile-first-ui/DR1-TP3.10/src/components/Header/index.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <h1 className={styles.title}>DR1 - TPs 3.10 a 3.16</h1>
+        <h1 className={styles.title}>DR1 - TPs 3.10</h1>
         <p className={styles.subtitle}>Projeto com CSS Modules</p>
       </div>
     </header>
