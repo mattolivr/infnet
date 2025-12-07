9c485687e7b17075fadca5c60badd715ea441f35
mattolivr <matheusdeoliveira310@gmail.com>
2025-12-07 18:43:37 -0300

    2-DR1-TP3.05

---
+++ 2-desenvolvimento-front-end/DR1-mobile-first-ui/DR1-TP3.05~3.07/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>DR1 - TPs 3.05 a 3.07</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At asperiores repellat illo quam vitae, eum perspiciatis dolores non itaque ut excepturi deleniti temporibus illum labore porro ducimus officiis omnis nemo.</p>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ 2-desenvolvimento-front-end/DR1-mobile-first-ui/DR1-TP3.05~3.07/src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Mobile First App</h1>
+    </header>
+  );
+}
