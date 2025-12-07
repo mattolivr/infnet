53626361f5c7d1c705e1cd90ed83c5d1965c0ace
mattolivr <matheusdeoliveira310@gmail.com>
2025-12-07 18:55:24 -0300

    2-DR1-TP3.08

---
+++ 2-desenvolvimento-front-end/DR1-mobile-first-ui/DR1-TP3.08~3.09/src/components/Card/index.jsx
@@ -0,0 +1,13 @@
+import styles from './Card.module.css';
+
+export default function Card({ image, title, description }) {
+  return (
+    <div className={styles.card}>
+      <img src={image} alt={title} className={styles.image} />
+      <div className={styles.content}>
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.description}>{description}</p>
+      </div>
+    </div>
+  );
+}
