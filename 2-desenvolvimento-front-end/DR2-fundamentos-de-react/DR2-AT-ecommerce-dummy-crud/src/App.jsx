73b4c3888f4b43ef80a65deb682a13ca71e11c18
mattolivr <matheusdeoliveira310@gmail.com>
2025-12-15 20:04:52 -0300

    2-DR1-AT.01

---
+++ 2-desenvolvimento-front-end/DR2-fundamentos-de-react/DR2-AT-ecommerce-dummy-crud/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>DR2 - AT - E-commerce Dummy CRUD</h1>
+      <h2>Sistema de Catálogo e Gestão de Produtos</h2>
+    </>
+  )
+}
+
+export default App
