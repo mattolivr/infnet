63bc45124d9769dc8b30586683ba3743e6b90cce
mattolivr <matheusdeoliveira310@gmail.com>
2025-12-15 20:27:23 -0300

    2-DR2-AT.04

--- 2-desenvolvimento-front-end/DR2-fundamentos-de-react/DR2-AT-ecommerce-dummy-crud/src/pages/product-list/index.jsx
+++ 2-desenvolvimento-front-end/DR2-fundamentos-de-react/DR2-AT-ecommerce-dummy-crud/src/pages/product-list/index.jsx
@@ -1,6 +1,22 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import style from './style.module.css';
 
 export default function ProductList() {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    const buscaProdutos = async () => {
+      try {
+        const response = await axios.get('https://dummyjson.com/products');
+        setProdutos(response.data.products);
+      } catch (error) {
+        new Error(error);
+      }
+    };
+    buscaProdutos();
+  }, []);
+
   return (
     <div>ProductList</div>
   );
