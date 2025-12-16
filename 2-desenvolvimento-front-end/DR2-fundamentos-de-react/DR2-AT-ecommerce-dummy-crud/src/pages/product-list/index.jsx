53fd40f3d0a7547fe2c2797ae6a0d61af1639c03
mattolivr <matheusdeoliveira310@gmail.com>
2025-12-15 21:34:36 -0300

    2-DR2-AT.10

--- 2-desenvolvimento-front-end/DR2-fundamentos-de-react/DR2-AT-ecommerce-dummy-crud/src/pages/product-list/index.jsx
+++ 2-desenvolvimento-front-end/DR2-fundamentos-de-react/DR2-AT-ecommerce-dummy-crud/src/pages/product-list/index.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import style from './style.module.css';
 
 export default function ProductList() {
   const [produtos, setProdutos] = useState([]);
   const [carregando, setCarregando] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const buscaProdutos = async () => {
@@ -30,10 +31,18 @@ export default function ProductList() {
       { produtos.map((produto) => (
           <div key={produto.id} className={style.produto}>
             <img src={produto.thumbnail} alt={produto.title} className={style.thumbnail} />
-            <h3>
-              <Link to={`/produtos/${produto.id}`}>{produto.title}</Link>
-            </h3>
-            <p className={style.preco}>Preço: R$ {produto.price.toString().replace(".", ",")}</p>
+            <div className={style.conteudo}>
+              <h3>
+                <Link to={`/produtos/${produto.id}`}>{produto.title}</Link>
+              </h3>
+              <p className={style.preco}>Preço: R$ {produto.price.toString().replace(".", ",")}</p>
+            </div>
+            <button 
+              className={style.botaoEditar}
+              onClick={() => navigate(`/novo/${produto.id}`)}
+            >
+              Editar
+            </button>
           </div>
         ))
       }
